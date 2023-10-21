540c6cb4397c94c007ebf5d53b30689e58e8e7e4
Oleg Ponomarev <o.sandman@gmail.com>
2023-10-21 23:02:06 +0300

    Refactoring rest controller exception handler - using response ProblemDetail object

--- src/main/java/net/osandman/votingforrestaurants/entity/RoleType.java
+++ src/main/java/net/osandman/votingforrestaurants/entity/RoleType.java
@@ -4,8 +4,7 @@ import org.springframework.security.core.GrantedAuthority;
 
 public enum RoleType implements GrantedAuthority {
     ADMIN,
-    REGULAR,
-    GUEST;
+    REGULAR;
 
     @Override
     public String getAuthority() {
--- src/main/java/net/osandman/votingforrestaurants/error/ErrorType.java
+++ src/main/java/net/osandman/votingforrestaurants/error/ErrorType.java
@@ -1,8 +1,24 @@
 package net.osandman.votingforrestaurants.error;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public enum ErrorType {
-    APP_ERROR,
-    DATA_NOT_FOUND,
-    DATA_ERROR,
-    VALIDATION_ERROR
+    APP_ERROR("Application error", HttpStatus.INTERNAL_SERVER_ERROR),
+    NOT_FOUND("Wrong data in request", HttpStatus.NOT_FOUND),
+    DATA_CONFLICT("Data integrity error", HttpStatus.CONFLICT),
+    BAD_DATA("Wrong data", HttpStatus.UNPROCESSABLE_ENTITY),
+    BAD_REQUEST("Bad request", HttpStatus.BAD_REQUEST),
+    UNAUTHORIZED("Request unauthorized", HttpStatus.UNAUTHORIZED),
+    FORBIDDEN("Request forbidden", HttpStatus.FORBIDDEN),
+    VALIDATION_ERROR("Validation error", HttpStatus.UNPROCESSABLE_ENTITY);
+
+    private final String title;
+    private final HttpStatus httpStatus;
+
+    ErrorType(String title, HttpStatus httpStatus) {
+        this.title = title;
+        this.httpStatus = httpStatus;
+    }
 }
